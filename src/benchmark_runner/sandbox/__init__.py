f99c19d5dbc0a3b9b478825339fe149fc40fa92c
Braden Wicker <bradenwicker@gmail.com>
2026-06-15 13:21:00 -0700

    remove: sandbox orchestrator and manifest generator (split to benchmark-orchestrator) (#13)

--- src/benchmark_runner/sandbox/__init__.py
+++
@@ -1,7 +0,0 @@
-from benchmark_runner.sandbox.backend import SandboxGenerationBackend
-from benchmark_runner.sandbox.bundle import AgentBundle, load_bundle
-from benchmark_runner.sandbox.protocols import ExecResultLike, SandboxLike
-from benchmark_runner.sandbox.contract import AgentContract, format_run_cmd
-from benchmark_runner.sandbox.orchestrator import evaluate_run, run_benchmark, score_run
-
-__all__ = ["AgentBundle", "AgentContract", "ExecResultLike", "SandboxGenerationBackend", "SandboxLike", "evaluate_run", "format_run_cmd", "load_bundle", "run_benchmark", "score_run"]
